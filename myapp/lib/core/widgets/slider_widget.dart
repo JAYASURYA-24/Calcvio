ccac5f88140ddac81c4473a059fad1136af0a92d
Jayasurya <ajay.p@fcsonline.co.in>
2025-06-13 17:23:05 +0530

    amount year slider widget changes

--- myapp/lib/core/widgets/slider_widget.dart
+++ myapp/lib/core/widgets/slider_widget.dart
@@ -733,6 +733,271 @@ class _SliderForPercentageState extends State<SliderForPercentage> {
   }
 }
 
+// class SliderForYearMonth extends StatefulWidget {
+//   final String title;
+//   final double value;
+//   final double min;
+//   final double max;
+//   final Function(double) onChanged;
+
+//   const SliderForYearMonth({
+//     super.key,
+//     required this.title,
+//     required this.value,
+//     required this.min,
+//     required this.max,
+//     required this.onChanged,
+//   });
+
+//   @override
+//   State<SliderForYearMonth> createState() => _SliderForYearMonthState();
+// }
+
+// class _SliderForYearMonthState extends State<SliderForYearMonth> {
+//   late TextEditingController _controller;
+//   bool _isEditing = false;
+//   bool isYearMode = true;
+//   late double _defaultValue;
+//   String? _errorText;
+
+//   @override
+//   @override
+//   void initState() {
+//     super.initState();
+//     _defaultValue = widget.value;
+//     _controller = TextEditingController();
+//     _updateControllerText(force: true);
+//     _controller.addListener(() {
+//       if (!_isEditing) return;
+//       final text = _controller.text;
+//       final parsed = double.tryParse(text);
+//       if (parsed == null) {
+//         setState(
+//           () =>
+//               _errorText =
+//                   'Enter ${isYearMode ? (widget.min / 12).round() : widget.min.toInt()} - ${isYearMode ? (widget.max / 12).round() : widget.max.toInt()}',
+//         );
+//         return;
+//       }
+
+//       final valueInMonths = isYearMode ? parsed * 12 : parsed;
+//       if (valueInMonths < widget.min || valueInMonths > widget.max) {
+//         setState(
+//           () =>
+//               _errorText =
+//                   'Enter ${isYearMode ? (widget.min / 12).round() : widget.min.toInt()} - ${isYearMode ? (widget.max / 12).round() : widget.max.toInt()}',
+//         );
+//         return;
+//       }
+
+//       setState(() => _errorText = null);
+//       widget.onChanged(valueInMonths);
+//     });
+//   }
+
+//   void _updateControllerText({bool force = false}) {
+//     final displayValue =
+//         isYearMode
+//             ? (widget.value / 12).toStringAsFixed(2)
+//             : widget.value.toStringAsFixed(2);
+
+//     if (force || !_isEditing) {
+//       _controller.text = displayValue;
+//     }
+//   }
+
+//   @override
+//   void didUpdateWidget(covariant SliderForYearMonth oldWidget) {
+//     super.didUpdateWidget(oldWidget);
+//     _updateControllerText();
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+
+//   String _formatValue(String input) {
+//     final trimmed = input.trim();
+
+//     if (trimmed.isEmpty || trimmed == '.' || trimmed.endsWith('.')) {
+//       return isYearMode
+//           ? (_defaultValue / 12).toStringAsFixed(2)
+//           : _defaultValue.toStringAsFixed(0);
+//     }
+
+//     final parsed = double.tryParse(trimmed);
+//     if (parsed == null) {
+//       return isYearMode
+//           ? (_defaultValue / 12).toStringAsFixed(2)
+//           : _defaultValue.toStringAsFixed(0);
+//     }
+
+//     final valueInMonths = isYearMode ? parsed * 12 : parsed;
+//     if (valueInMonths < widget.min || valueInMonths > widget.max) {
+//       return isYearMode
+//           ? (_defaultValue / 12).toStringAsFixed(2)
+//           : _defaultValue.toStringAsFixed(0);
+//     }
+
+//     return isYearMode
+//         ? (parsed % 1 == 0
+//             ? parsed.toInt().toString()
+//             : parsed.toStringAsFixed(2))
+//         : parsed.toStringAsFixed(0);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final displayValue = isYearMode ? widget.value / 12 : widget.value;
+//     final divisions =
+//         isYearMode
+//             ? ((widget.max - widget.min) / 0.01).round()
+//             : (widget.max - widget.min).round();
+
+//     return Column(
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       children: [
+//         Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//           children: [
+//             Text(
+//               widget.title,
+//               style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+//             ),
+//             Row(
+//               children: [
+//                 SizedBox(
+//                   width: 73,
+//                   child: Focus(
+//                     onFocusChange: (hasFocus) {
+//                       setState(() => _isEditing = hasFocus);
+//                       if (!hasFocus) {
+//                         final formatted = _formatValue(_controller.text);
+//                         final parsed = double.tryParse(formatted);
+
+//                         setState(() {
+//                           _errorText = null;
+//                           _controller.text = formatted;
+//                           _controller.selection = TextSelection.collapsed(
+//                             offset: formatted.length,
+//                           );
+//                         });
+
+//                         if (parsed != null) {
+//                           final valueInMonths =
+//                               isYearMode ? parsed * 12 : parsed;
+//                           widget.onChanged(valueInMonths);
+//                         }
+//                       }
+//                     },
+
+//                     child: TextField(
+//                       cursorColor: AppColors.lightChartLine,
+//                       enableInteractiveSelection: false,
+//                       style: TextStyle(
+//                         color: AppColors.lightChartLine,
+//                         fontSize: 14,
+//                       ),
+//                       textAlign: TextAlign.end,
+//                       controller: _controller,
+//                       keyboardType: TextInputType.number,
+//                       decoration: InputDecoration(
+//                         filled: true,
+//                         fillColor: AppColors.lightSliderInactive,
+//                         isDense: true,
+//                         contentPadding: const EdgeInsets.symmetric(
+//                           vertical: 4,
+//                           horizontal: 8,
+//                         ),
+//                         enabledBorder: OutlineInputBorder(
+//                           borderRadius: BorderRadius.circular(15),
+//                           borderSide: BorderSide(color: Colors.transparent),
+//                         ),
+//                         focusedBorder: OutlineInputBorder(
+//                           borderRadius: BorderRadius.circular(15),
+//                           borderSide: BorderSide(color: Colors.transparent),
+//                         ),
+//                         errorBorder: OutlineInputBorder(
+//                           borderRadius: BorderRadius.circular(15),
+//                           borderSide: BorderSide(color: Colors.red),
+//                         ),
+//                         focusedErrorBorder: OutlineInputBorder(
+//                           borderRadius: BorderRadius.circular(15),
+//                           borderSide: BorderSide(color: Colors.red),
+//                         ),
+//                         errorText: _errorText,
+//                         errorStyle: TextStyle(fontSize: 10),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//                 SizedBox(width: 8),
+//                 ToggleButtons(
+//                   fillColor: AppColors.lightChartFill,
+//                   selectedColor: AppColors.lightChartFill,
+//                   borderRadius: BorderRadius.circular(5),
+//                   constraints: BoxConstraints(minWidth: 0, minHeight: 18),
+//                   isSelected: [!isYearMode, isYearMode],
+//                   onPressed: (index) {
+//                     setState(() {
+//                       isYearMode = index == 1;
+//                       _updateControllerText(force: true);
+//                     });
+//                   },
+//                   children: const [
+//                     Padding(
+//                       padding: EdgeInsets.symmetric(horizontal: 7),
+//                       child: Text(
+//                         'M',
+//                         style: TextStyle(color: AppColors.lightChartLine),
+//                       ),
+//                     ),
+//                     Padding(
+//                       padding: EdgeInsets.symmetric(horizontal: 9),
+//                       child: Text(
+//                         'Y',
+//                         style: TextStyle(color: AppColors.lightChartLine),
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//         Row(
+//           children: [
+//             Expanded(
+//               child: SliderTheme(
+//                 data: SliderTheme.of(context).copyWith(
+//                   tickMarkShape:
+//                       SliderTickMarkShape.noTickMark, // Hides tick marks
+//                 ),
+//                 child: Slider(
+//                   padding: EdgeInsets.fromLTRB(10, 8, 10, 10),
+//                   value: displayValue,
+//                   min: isYearMode ? widget.min / 12 : widget.min,
+//                   max: isYearMode ? widget.max / 12 : widget.max,
+//                   activeColor: AppColors.lightChartLine,
+//                   inactiveColor: AppColors.lightSliderInactive,
+
+//                   onChanged: (val) {
+//                     final valueInMonths = isYearMode ? val * 12 : val;
+//                     widget.onChanged(valueInMonths);
+//                     _controller.text = val.toStringAsFixed(isYearMode ? 2 : 0);
+//                   },
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ],
+//     );
+//   }
+// }
+
 class SliderForYearMonth extends StatefulWidget {
   final String title;
   final double value;
@@ -761,7 +1026,6 @@ class _SliderForYearMonthState extends State<SliderForYearMonth> {
   String? _errorText;
 
   @override
-  @override
   void initState() {
     super.initState();
     _defaultValue = widget.value;
@@ -773,9 +1037,7 @@ class _SliderForYearMonthState extends State<SliderForYearMonth> {
       final parsed = double.tryParse(text);
       if (parsed == null) {
         setState(
-          () =>
-              _errorText =
-                  'Enter ${isYearMode ? (widget.min / 12).round() : widget.min.toInt()} - ${isYearMode ? (widget.max / 12).round() : widget.max.toInt()}',
+          () => _errorText = 'Enter ${_displayMin()} - ${_displayMax()}',
         );
         return;
       }
@@ -783,9 +1045,7 @@ class _SliderForYearMonthState extends State<SliderForYearMonth> {
       final valueInMonths = isYearMode ? parsed * 12 : parsed;
       if (valueInMonths < widget.min || valueInMonths > widget.max) {
         setState(
-          () =>
-              _errorText =
-                  'Enter ${isYearMode ? (widget.min / 12).round() : widget.min.toInt()} - ${isYearMode ? (widget.max / 12).round() : widget.max.toInt()}',
+          () => _errorText = 'Enter ${_displayMin()} - ${_displayMax()}',
         );
         return;
       }
@@ -796,15 +1056,32 @@ class _SliderForYearMonthState extends State<SliderForYearMonth> {
   }
 
   void _updateControllerText({bool force = false}) {
+    final value = isYearMode ? widget.value / 12 : widget.value;
     final displayValue =
         isYearMode
-            ? (widget.value / 12).toStringAsFixed(2)
-            : widget.value.toStringAsFixed(2);
+            ? (value % 1 == 0
+                ? value.toInt().toString()
+                : value.toStringAsFixed(1))
+            : value.toStringAsFixed(0);
 
     if (force || !_isEditing) {
       _controller.text = displayValue;
     }
   }
+
+  String _displayMin() =>
+      isYearMode
+          ? ((widget.min / 12) % 1 == 0
+              ? (widget.min / 12).toInt().toString()
+              : (widget.min / 12).toStringAsFixed(1))
+          : widget.min.toStringAsFixed(0);
+
+  String _displayMax() =>
+      isYearMode
+          ? ((widget.max / 12) % 1 == 0
+              ? (widget.max / 12).toInt().toString()
+              : (widget.max / 12).toStringAsFixed(1))
+          : widget.max.toStringAsFixed(0);
 
   @override
   void didUpdateWidget(covariant SliderForYearMonth oldWidget) {
@@ -822,29 +1099,38 @@ class _SliderForYearMonthState extends State<SliderForYearMonth> {
     final trimmed = input.trim();
 
     if (trimmed.isEmpty || trimmed == '.' || trimmed.endsWith('.')) {
+      final value = isYearMode ? _defaultValue / 12 : _defaultValue;
       return isYearMode
-          ? (_defaultValue / 12).toStringAsFixed(2)
-          : _defaultValue.toStringAsFixed(0);
+          ? (value % 1 == 0
+              ? value.toInt().toString()
+              : value.toStringAsFixed(1))
+          : value.toStringAsFixed(0);
     }
 
     final parsed = double.tryParse(trimmed);
     if (parsed == null) {
+      final value = isYearMode ? _defaultValue / 12 : _defaultValue;
       return isYearMode
-          ? (_defaultValue / 12).toStringAsFixed(2)
-          : _defaultValue.toStringAsFixed(0);
+          ? (value % 1 == 0
+              ? value.toInt().toString()
+              : value.toStringAsFixed(1))
+          : value.toStringAsFixed(0);
     }
 
     final valueInMonths = isYearMode ? parsed * 12 : parsed;
     if (valueInMonths < widget.min || valueInMonths > widget.max) {
+      final value = isYearMode ? _defaultValue / 12 : _defaultValue;
       return isYearMode
-          ? (_defaultValue / 12).toStringAsFixed(2)
-          : _defaultValue.toStringAsFixed(0);
+          ? (value % 1 == 0
+              ? value.toInt().toString()
+              : value.toStringAsFixed(1))
+          : value.toStringAsFixed(0);
     }
 
     return isYearMode
         ? (parsed % 1 == 0
             ? parsed.toInt().toString()
-            : parsed.toStringAsFixed(2))
+            : parsed.toStringAsFixed(1))
         : parsed.toStringAsFixed(0);
   }
 
@@ -853,7 +1139,7 @@ class _SliderForYearMonthState extends State<SliderForYearMonth> {
     final displayValue = isYearMode ? widget.value / 12 : widget.value;
     final divisions =
         isYearMode
-            ? ((widget.max - widget.min) / 0.01).round()
+            ? ((widget.max / 12 - widget.min / 12) / 0.5).round()
             : (widget.max - widget.min).round();
 
     return Column(
@@ -892,53 +1178,58 @@ class _SliderForYearMonthState extends State<SliderForYearMonth> {
                         }
                       }
                     },
-
                     child: TextField(
                       cursorColor: AppColors.lightChartLine,
                       enableInteractiveSelection: false,
-                      style: TextStyle(
+                      style: const TextStyle(
                         color: AppColors.lightChartLine,
-                        fontSize: 14,
+                        fontSize: 16,
                       ),
                       textAlign: TextAlign.end,
                       controller: _controller,
-                      keyboardType: TextInputType.number,
+                      keyboardType: const TextInputType.numberWithOptions(
+                        decimal: true,
+                      ),
                       decoration: InputDecoration(
                         filled: true,
                         fillColor: AppColors.lightSliderInactive,
                         isDense: true,
                         contentPadding: const EdgeInsets.symmetric(
-                          vertical: 4,
+                          vertical: 2,
                           horizontal: 8,
                         ),
                         enabledBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(15),
-                          borderSide: BorderSide(color: Colors.transparent),
+                          borderSide: const BorderSide(
+                            color: Colors.transparent,
+                          ),
                         ),
                         focusedBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(15),
-                          borderSide: BorderSide(color: Colors.transparent),
+                          borderSide: const BorderSide(
+                            color: Colors.transparent,
+                          ),
                         ),
                         errorBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(15),
-                          borderSide: BorderSide(color: Colors.red),
+                          borderSide: const BorderSide(color: Colors.red),
                         ),
                         focusedErrorBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(15),
-                          borderSide: BorderSide(color: Colors.red),
+                          borderSide: const BorderSide(color: Colors.red),
                         ),
                         errorText: _errorText,
-                        errorStyle: TextStyle(fontSize: 10),
+                        errorStyle: const TextStyle(),
                       ),
                     ),
                   ),
                 ),
-                SizedBox(width: 8),
+                const SizedBox(width: 8),
                 ToggleButtons(
                   fillColor: AppColors.lightChartFill,
                   selectedColor: AppColors.lightChartFill,
                   borderRadius: BorderRadius.circular(5),
-                  constraints: BoxConstraints(minWidth: 0, minHeight: 18),
+                  constraints: const BoxConstraints(minWidth: 0, minHeight: 10),
                   isSelected: [!isYearMode, isYearMode],
                   onPressed: (index) {
                     setState(() {
@@ -971,22 +1262,26 @@ class _SliderForYearMonthState extends State<SliderForYearMonth> {
           children: [
             Expanded(
               child: SliderTheme(
-                data: SliderTheme.of(context).copyWith(
-                  tickMarkShape:
-                      SliderTickMarkShape.noTickMark, // Hides tick marks
-                ),
+                data: SliderTheme.of(
+                  context,
+                ).copyWith(tickMarkShape: SliderTickMarkShape.noTickMark),
                 child: Slider(
                   padding: EdgeInsets.fromLTRB(10, 8, 10, 10),
+                  divisions: divisions,
                   value: displayValue,
                   min: isYearMode ? widget.min / 12 : widget.min,
                   max: isYearMode ? widget.max / 12 : widget.max,
                   activeColor: AppColors.lightChartLine,
                   inactiveColor: AppColors.lightSliderInactive,
-
                   onChanged: (val) {
                     final valueInMonths = isYearMode ? val * 12 : val;
                     widget.onChanged(valueInMonths);
-                    _controller.text = val.toStringAsFixed(isYearMode ? 2 : 0);
+                    _controller.text =
+                        isYearMode
+                            ? (val % 1 == 0
+                                ? val.toInt().toString()
+                                : val.toStringAsFixed(1))
+                            : val.toStringAsFixed(0);
                   },
                 ),
               ),
